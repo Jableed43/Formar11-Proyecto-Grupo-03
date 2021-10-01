e0b32df38b019845009c3587dfbe2e1ae5b0fec8
Jableed43 <javiernehuenlopez@gmail.com>
2021-10-01 12:57:03 -0300

    cambios en el home, menu integrado

--- site/src/public/javascripts/menu-bar.js
+++ site/src/public/javascripts/menu-bar.js
@@ -20,4 +20,6 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+
 }
