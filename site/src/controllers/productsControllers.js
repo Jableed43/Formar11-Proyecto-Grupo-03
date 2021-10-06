221231aef52b3db0769bd431121600d3f667d944
Yamila-Vecces <yamilavecces@gmail.com>
2021-10-06 00:44:43 -0300

    Products y detalle listos

--- site/src/controllers/productsControllers.js
+++ site/src/controllers/productsControllers.js
@@ -1,6 +1,51 @@
-let controller = {
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+const controller = {
+    // Root - Show all products
+	index: (req, res) => {
+		res.render('products', {products})
+	},
+
     detalle:(req, res, next) => {
-    res.render('products/detalle-producto')},
+        const {id} = req.params
+		const productDetail = products.find(e => e.id === +id)
+        res.render('products/detalle-producto', {productDetail})
+    },
+    // Create - Form to create
+	create: (req, res) => {
+		res.render('create')
+	},
+    // Create -  Method to store
+	store: (req, res) => {
+        const object = req.body
+		object.id = products.length
+		products.push(products)
+		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2))
+		res.redirect('/products/detail/${product.id}')
+	},
+    // Update - Form to edit
+	edit: (req, res) => {
+		products.find(e => e.id === req.params.id)
+		res.render('edit', {products})
+	},
+	
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		products = products.filter(product => product.id === +req.params.id)
+
+		fs.writeFileSync(productsFilePath, JSON.stringify(products))
+
+        res.redirect('/')
+	}
+
 }
+
+
 
 module.exports = controller
--- site/src/routes/products.js
+++ site/src/routes/products.js
@@ -2,7 +2,24 @@ var express = require('express');
 var router = express.Router();
 let productsController = require('../controllers/productsControllers')
 
+/*** GET ALL PRODUCTS ***/ 
+router.get('/', productsController.index); 
+
 /* GET detalle de productos */
-router.get('/detail', productsController.detalle);
+router.get('/detail/:id', productsController.detalle);
+
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/create', productsController.create); 
+router.post('/', productsController.store); 
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id', productsController.edit); 
+
+
+
+/*** DELETE ONE PRODUCT***/ 
+router.delete('/delete/:id', productsController.destroy); 
+
+
 
 module.exports = router;
