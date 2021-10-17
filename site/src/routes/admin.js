11b22f35a89ade18f3c3c80498334041b2926bb3
Jableed43 <javiernehuenlopez@gmail.com>
2021-10-17 19:34:54 -0300

    agregado multer al proyecto

--- site/src/routes/admin.js
+++ site/src/routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 let adminController = require ('../controllers/adminControllers')
 
+
 /* GET home page. */
 router.get('/', adminController.admin);
 
--- site/src/routes/products.js
+++ site/src/routes/products.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 let productsController = require('../controllers/productsControllers')
+const multer = require('multer');
+
 
 /* GET detalle de productos */
 router.get('/detail', productsController.detalle);
