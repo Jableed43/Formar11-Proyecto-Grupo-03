483cc07acb430619184b418af1ef2b7930992ecc
Ginevrana <florenciateresita@gmail.com>
2021-10-19 22:11:46 -0300

    Se modifica timer menu

--- site/src/public/javascripts/menu-bar.js
+++ site/src/public/javascripts/menu-bar.js
@@ -20,6 +20,5 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
 
 }
