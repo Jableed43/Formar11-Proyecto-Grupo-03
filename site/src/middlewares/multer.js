78bc4f5743a0f8e8640939a0b88dc3304b7bc3aa
Ginevrana <florenciateresita@gmail.com>
2021-10-21 22:14:32 -0300

    MULTER FUNCIONANDO

--- site/src/middlewares/multer.js
+++ site/src/middlewares/multer.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '../src/public/images/users')
+      cb(null,'src/public/images/users')
     },
     filename: function (req, file, cb) {
       cb(null, `img-${Date.now()}${path.extname(file.originalname)}`)
