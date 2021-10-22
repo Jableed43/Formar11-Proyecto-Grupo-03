f0e1309a8fa8a72f43c314dc14f0aabfad38119c
Ginevrana <florenciateresita@gmail.com>
2021-10-21 21:49:00 -0300

    se suben actualizaciones multer, deja de funcionar, se arregla 404

--- site/src/controllers/usersControllers.js
+++ site/src/controllers/usersControllers.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
+
 const usersFilePath = path.join(__dirname, '../data/users.json');
 let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
--- site/src/middlewares/multer.js
+++ site/src/middlewares/multer.js
@@ -1,10 +1,9 @@
 const multer = require('multer');
-const upload = multer({dest:'../public/images/users'})
 const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '../public/images/users')
+      cb(null, '../src/public/images/users')
     },
     filename: function (req, file, cb) {
       cb(null, `img-${Date.now()}${path.extname(file.originalname)}`)
