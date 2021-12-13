47ea879d50d1783561779fedd3427a15f845aef9
Jableed43 <javiernehuenlopez@gmail.com>
2021-12-13 18:09:36 -0300

    añadida validacion register y login de front end, bugs: no aparecen errores y no me permite ingresar a pagina de register

--- site/public/javascripts/validacion-login.js
+++ site/public/javascripts/validacion-login.js
@@ -1,1 +1,32 @@
-console.log(hola);
+const qs = (tag) => {
+    return document.querySelector(tag)
+}
+
+const divErr = document.querySelector(".errores ul")
+
+window.addEventListener("load", function() {
+
+
+    let formulario = qs("form");
+
+    formulario.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        let errores = []
+
+        const campoEmail = qs("#user");
+
+        if(campoEmail.value === "") {
+            errores.push("El campo de nombre tiene que estar completo")
+        }
+        const campoPassword = qs("#password")
+
+        if(campoPassword.value === "") {
+            errores.push("El campo de contraseña tiene que estar completo")
+        }
+
+
+    })
+
+})
+
---
+++ site/public/javascripts/validacion-register.js
@@ -0,0 +1,79 @@
+const qs = (tag) => {
+    return document.querySelector(tag)
+}
+
+const divErr = document.querySelector(".errores ul")
+
+window.addEventListener("load", function() {
+
+
+    let formulario = qs("form");
+
+    formulario.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        let errores = []
+
+        const campoUser = qs("#user");
+
+        if(campoUser.value === "") {
+            errores.push("El el nombre tiene que estar completo")
+        }
+
+        if(campoUser.value.length <= 1 ) {
+            errores.push("El nombre debe poseer al menos dos caracteres")
+        }
+
+        const campoEmail = qs("#email");
+
+        if(campoEmail.value === "") {
+            errores.push("El mail tiene que estar completo")
+        }
+
+        const campoPassword1 = qs("#p1")
+
+        if(campoPassword1.value === "") {
+            errores.push("La contraseña tiene que estar completa")
+        }
+
+        if(campoPassword1.value.length <= 7 ) {
+            errores.push("La contraseña debe poseer al menos ocho caracteres")
+        }
+
+        const campoPassword2 = qs("#p2")
+
+        if(campoPassword2.value === "") {
+            errores.push("La contraseña tiene que estar completa")
+        }
+
+        if(campoPassword2.value.length <= 7 ) {
+            errores.push("La contraseña debe poseer al menos ocho caracteres")
+        }
+
+        if(campoPassword2.value !== campoPassword1.value) {
+            errores.push("Ambas contraseñas deben ser iguales")
+        }
+
+        const campoSexo = qs("#sexo")
+
+        if(campoSexo.value === "") {
+            errores.push("Ingrese su sexo porfavor")
+        }
+
+        const campoProvincia = qs("#provincia")
+
+        if(campoProvincia.value === "") {
+            errores.push("Ingrese provincia que habita")
+        }
+
+        
+
+        const campoImg = qs("#img")
+
+
+        
+
+    })
+
+})
+
