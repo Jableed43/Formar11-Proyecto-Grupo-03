c96b05efab1ae1e5a6b65b99fecce899525a146c
Jableed43 <javiernehuenlopez@gmail.com>
2021-12-21 21:03:17 -0300

    cambios en input email de register, pasa a ser text

--- site/public/javascripts/validacion-login.js
+++ site/public/javascripts/validacion-login.js
@@ -50,7 +50,6 @@ window.addEventListener("load", function() {
 
 
     formulario.addEventListener("submit", function(e) {
-        e.preventDefault();
 
         for (let i = 0; i < inputs.length; i++) {
             if (inputs[i].value === "") {
