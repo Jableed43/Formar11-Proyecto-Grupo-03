755f3a332267ce20165b91feabc1676ca13e3368
Jableed43 <javiernehuenlopez@gmail.com>
2022-01-12 17:47:35 -0300

    agregado 500

--- site/src/app.js
+++ site/src/app.js
@@ -67,8 +67,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  // res.status(err.status || 500);
-  // res.render('500');
+  res.status(err.status || 500);
+  res.render('500');
 });
 
 module.exports = app;
